be376d9ad44313c75c5e8c86030fb51199a49f65
FAlkhanaizi <fatema.alkhanaizi@durham.ac.uk>
2019-01-16 21:17:37 +0000

    Update test-omp.c
    testing nested loop parallel

--- test-omp.c
+++ test-omp.c
@@ -329,12 +329,15 @@ void updateBodyInner() {
             fx = 0.0;
             fy = 0.0;
             fz = 0.0;
+            std::cout << NumberOfBodies << " number of bodies\n" << std::endl;
             //reference for step2 http://phys.ubbcluj.ro/~tbeu/MD/C2_for.pdf
             // http://courses.cs.vt.edu/cs4414/S15/LECTURES/MolecularDynamics.pdf
             // http://phycomp.technion.ac.il/~talimu/md2.html
             // the last r is squared because we break the force down to x,y and z components
 #pragma omp for private(xi, yi, zi, dx, dy, dz, r2, fr2, fr6, F) reduction(min:tempMin) reduction(+:fx,fy,fz)
             for (int j = 0; j < NumberOfBodies; j++) {
+                std::cout << i << " i " << j << " j\n" << std::endl;
+
                 if (i == j) continue;
 
                 dx = xi - x[j][0];
@@ -363,10 +366,12 @@ void updateBodyInner() {
                 fz += dz * F;
 
                 tempMin = std::min(tempMin, r2);
+                std::cout << r2 << " " << tempMin << "inner minimum distance\n" << std::endl;
             }
 
-#pragma omp critical
             minDx = std::min(minDx, tempMin);
+
+            std::cout << minDx << "minimum distance" << std::endl;
 
             force[i][0] = fx;
             force[i][1] = fy;
